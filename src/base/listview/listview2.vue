bf55684f3266658128dd466a531610939cfc77a2
Robert_Chiao <935065201@qq.com>
2018-04-12 11:36:21 +0800

    4.12更改

--- src/base/listview/listview2.vue
+++ src/base/listview/listview2.vue
@@ -1,31 +1,34 @@
 <template>
-    <scroll class="listview" :data="data" :listenScroll="listenScroll" :probeType="probeType" @scroll="scroll" ref="listview">
-        <ul class="list-group">
-            <li v-for="group in data" ref='listGroup'>
-                <h2 class="list-group-title">{{group.title}}</h2>
-                <ul>
-                    <li v-for="item in group.items" class="list-group-item">
-                        <img class="avatar" v-lazy="item.avatar" />
-                        <span class="name">{{item.name}}</span>
-                    </li>
-                </ul>
-            </li>
+  <scroll class="listview" :data="data" :listenScroll="listenScroll" :probeType="probeType" @scroll="scroll" ref="listview">
+    <ul class="list-group">
+      <li v-for="group in data" ref='listGroup'>
+        <h2 class="list-group-title">{{group.title}}</h2>
+        <ul>
+          <li v-for="item in group.items" class="list-group-item">
+            <img class="avatar" v-lazy="item.avatar" />
+            <span class="name">{{item.name}}</span>
+          </li>
         </ul>
-        <div class="list-shortcut">
-            <ul>
-                <li v-for="(item,index) in shortcutList" class="item" :class="{'current':currentIndex === index}" @touchstart="onShortcutTouchStart" :data-index="index" @touchmove.stop.prevent="onShortcutTouchMove">
-                    {{item}}
-                </li>
-            </ul>
-        </div>
-    </scroll>
+      </li>
+    </ul>
+    <div class="list-shortcut">
+      <ul>
+        <li v-for="(item,index) in shortcutList" class="item" :class="{'current':currentIndex === index}" @touchstart="onShortcutTouchStart" :data-index="index" @touchmove.stop.prevent="onShortcutTouchMove">
+          {{item}}
+        </li>
+      </ul>
+    </div>
+    <div class="list-fixed" v-show="fixedTitle" ref="fixed">
+      <h1 class="fixed-title">{{fixedTitle}}</h1>
+    </div>
+  </scroll>
 </template>
 
 <script type="text/ecmascript-6">
 import Scroll from "base/scroll/scroll2";
 import { getData } from "common/js/dom2";
 const ANCHOR_HEIGHT = 18;
-
+const TITLE_HEIGHT = 30;
 export default {
   created() {
     this.touch = {};
@@ -36,7 +39,8 @@ export default {
   data() {
     return {
       scrollY: -1,
-      currentIndex: 0
+      currentIndex: 0,
+      diff: -1
     };
   },
   watch: {
@@ -47,7 +51,6 @@ export default {
     },
     scrollY(newY) {
       const listHeight = this.listHeight;
-      console.log(typeof this.scrollY);
 
       //当-newY>0的时候
       if (newY > 0) {
@@ -60,11 +63,21 @@ export default {
         let height2 = listHeight[i + 1];
         if (-newY >= height1 && -newY < height2) {
           this.currentIndex = i;
+          this.diff = height2 + newY;
           return;
         }
       }
       // 当滚动到底部，且-newY大于最后一个元素的上限
       this.currentIndex = listHeight.length - 2;
+    },
+    diff(newVal) {
+      let fixTop =
+        newVal > 0 && newVal < TITLE_HEIGHT ? newVal - TITLE_HEIGHT : 0;
+      if (this.fixTop === fixTop) {
+        return;
+      }
+      this.fixTop = fixTop;
+      this.$refs.fixed.style.transform = `translate3d(0,${fixTop}px,0)`;
     }
   },
   props: {
@@ -78,6 +91,14 @@ export default {
       return this.data.map(group => {
         return group.title.substr(0, 1);
       });
+    },
+    fixedTitle() {
+      if (this.scrollY > 0) {
+        return "";
+      }
+      return this.data[this.currentIndex]
+        ? this.data[this.currentIndex].title
+        : "";
     }
   },
   methods: {
@@ -111,9 +132,10 @@ export default {
       }
     },
     _scrollTo(index) {
-      //   this.currentIndex = index;
+      if (index < 0) {
+        index = 0;
+      }
       this.scrollY = -this.listHeight[index];
-      console.log(typeof this.scrollY);
       this.$refs.listview.scrollToElement(this.$refs.listGroup[index], 0);
     }
   },
@@ -127,89 +149,89 @@ export default {
 @import '~common/stylus/variable';
 
 .listview {
-    position: relative;
+  position: relative;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  background: $color-background;
+
+  .list-group {
+    padding-bottom: 30px;
+
+    .list-group-title {
+      height: 30px;
+      line-height: 30px;
+      padding-left: 20px;
+      font-size: $font-size-small;
+      color: $color-text-l;
+      background: $color-highlight-background;
+    }
+
+    .list-group-item {
+      display: flex;
+      align-items: center;
+      padding: 20px 0 0 30px;
+
+      .avatar {
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+      }
+
+      .name {
+        margin-left: 20px;
+        color: $color-text-l;
+        font-size: $font-size-medium;
+      }
+    }
+  }
+
+  .list-shortcut {
+    position: absolute;
+    z-index: 30;
+    right: 0;
+    top: 50%;
+    transform: translateY(-50%);
+    width: 20px;
+    padding: 20px 0;
+    border-radius: 10px;
+    text-align: center;
+    background: $color-background-d;
+    font-family: Helvetica;
+
+    .item {
+      padding: 3px;
+      line-height: 1;
+      color: $color-text-l;
+      font-size: $font-size-small;
+
+      &.current {
+        color: $color-theme;
+      }
+    }
+  }
+
+  .list-fixed {
+    position: absolute;
+    top: 0;
+    left: 0;
     width: 100%;
-    height: 100%;
-    overflow: hidden;
-    background: $color-background;
 
-    .list-group {
-        padding-bottom: 30px;
-
-        .list-group-title {
-            height: 30px;
-            line-height: 30px;
-            padding-left: 20px;
-            font-size: $font-size-small;
-            color: $color-text-l;
-            background: $color-highlight-background;
-        }
-
-        .list-group-item {
-            display: flex;
-            align-items: center;
-            padding: 20px 0 0 30px;
-
-            .avatar {
-                width: 50px;
-                height: 50px;
-                border-radius: 50%;
-            }
-
-            .name {
-                margin-left: 20px;
-                color: $color-text-l;
-                font-size: $font-size-medium;
-            }
-        }
+    .fixed-title {
+      height: 30px;
+      line-height: 30px;
+      padding-left: 20px;
+      font-size: $font-size-small;
+      color: $color-text-l;
+      background: $color-highlight-background;
     }
+  }
 
-    .list-shortcut {
-        position: absolute;
-        z-index: 30;
-        right: 0;
-        top: 50%;
-        transform: translateY(-50%);
-        width: 20px;
-        padding: 20px 0;
-        border-radius: 10px;
-        text-align: center;
-        background: $color-background-d;
-        font-family: Helvetica;
-
-        .item {
-            padding: 3px;
-            line-height: 1;
-            color: $color-text-l;
-            font-size: $font-size-small;
-
-            &.current {
-                color: $color-theme;
-            }
-        }
-    }
-
-    .list-fixed {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-
-        .fixed-title {
-            height: 30px;
-            line-height: 30px;
-            padding-left: 20px;
-            font-size: $font-size-small;
-            color: $color-text-l;
-            background: $color-highlight-background;
-        }
-    }
-
-    .loading-container {
-        position: absolute;
-        width: 100%;
-        top: 50%;
-        transform: translateY(-50%);
-    }
+  .loading-container {
+    position: absolute;
+    width: 100%;
+    top: 50%;
+    transform: translateY(-50%);
+  }
 }
 </style>
